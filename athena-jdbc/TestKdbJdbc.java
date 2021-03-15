1be354818749442afdeb835350b3e864ebf6d8f4
tkcmada <tokutaro@gmail.com>
2021-03-15 06:40:27 +0000

    update TestKdbJdbc

--- athena-jdbc/TestKdbJdbc.java
+++ athena-jdbc/TestKdbJdbc.java
@@ -7,10 +7,20 @@ public class TestKdbJdbc {
 		Connection h = DriverManager.getConnection("jdbc:q:127.0.0.1:5001", "", "");
 		System.out.println("connected. v1");
 		Statement stmt = h.createStatement();
-		ResultSet rs = stmt.executeQuery("select * from t2");
+		ResultSet rs = stmt.executeQuery("select * from t6");
 		while(rs.next()) {
+			System.out.println("---");
 			Object obj = rs.getObject(1);
-			System.out.println(obj + (obj == null ? "null" : obj.getClass().getName()));
+			if(obj == null)
+				System.out.println("null");
+			else if(obj.getClass().isArray()) {
+				Object[] a = (Object[])obj;
+				for(int i = 0; i < a.length; i++) {
+					System.out.println(i + ":" + a[i] + ":" + (a[i] == null ? "null" : a[i].getClass().getName()));
+				}
+			}
+			else
+				System.out.println(obj + ":" + obj.getClass().getName());
 		}
 	}
 }
